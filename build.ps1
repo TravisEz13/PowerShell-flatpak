5d89da063b8880597de99d08ecd2005b956bb3f7
TheMentor <the-mentor@users.noreply.github.com>
2021-05-05 03:21:56 +0300

    Flatpak Enchancments (#6)

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
+#!/usr/bin/env pwsh
 [CmdletBinding(DefaultParameterSetName = 'Build')]
 param(
     [Parameter(Mandatory, ParameterSetName = 'Bootstrap')]
@@ -5,8 +6,14 @@ param(
 
     [Parameter(Mandatory, ParameterSetName = 'Build')]
     [switch] $Build,
-    [Parameter(ParameterSetName = 'Build')]
-    [switch] $Run
+    [Parameter(Mandatory, ParameterSetName = 'Install')]
+    [switch] $Install,
+    [Parameter(Mandatory, ParameterSetName = 'Uninstall')]
+    [switch] $Uninstall,
+    [Parameter(Mandatory, ParameterSetName = 'Run')]
+    [switch] $Run,
+    [Parameter()]
+    [ValidateSet('Stable','Preview','Servicing')][string] $Release = 'Stable'
 )
 
 if (!$IsLinux) {
@@ -15,23 +22,94 @@ if (!$IsLinux) {
 
 $metadataUrl = 'https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/metadata.json'
 $metadata = Invoke-RestMethod -Uri $metadataUrl
-$stableReleaseTag = $metadata.StableReleaseTag
-$version = $stableReleaseTag -replace '^v'
+
+switch ($Release) {
+    'Stable'{$ReleaseTag = $metadata.StableReleaseTag}
+    'Preview'{$ReleaseTag = $metadata.PreviewReleaseTag}
+    'Servicing'{$ReleaseTag = $metadata.ServicingReleaseTag}
+    default {throw "Incorrect Release Specified: $Release"}
+}
+
+$version = $ReleaseTag -replace '^v'
 $packageUrl = "https://github.com/PowerShell/PowerShell/releases/download/v$version/powershell-$version-linux-x64.tar.gz"
 
 if ($Bootstrap.IsPresent) {
-    sudo apt update
-    sudo apt install flatpak flatpak-builder
+    #Get Linux System Information
+    $LinuxInfo = Get-Content /etc/os-release | ConvertFrom-StringData
+    $Environment = [PSCustomObject]@{
+        IsUbuntu            = $LinuxInfo.Id -match 'ubuntu'
+        IsCentOS            = $LinuxInfo.Id -match 'centos'
+        IsFedora            = $LinuxInfo.Id -match 'fedora'
+        IsOpenSUSE          = $LinuxInfo.Id -match 'opensuse'
+    }
+
+    if($Environment.IsUbuntu){
+        sudo apt update
+        sudo apt install -y flatpak flatpak-builder
+    }
+    elseif($Environment.IsFedora){
+        sudo dnf check-update
+        sudo dnf -y install flatpak flatpak-builder
+    }
+    elseif($Environment.IsOpenSUSE){
+        sudo zypper refresh
+        sudo zypper -n install flatpak flatpak-builder
+    }
+    
     sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    sudo flatpak -y install flathub org.freedesktop.Platform//19.08 org.freedesktop.Sdk//19.08
+    sudo flatpak -y install flathub org.freedesktop.Platform//20.08 org.freedesktop.Sdk//20.08
 }
 
 if ($Build.IsPresent) {
     Push-Location
     Set-Location $PSScriptRoot
+
+    try {
+        Invoke-WebRequest -Uri $packageUrl -OutFile "/tmp/powershell-$version-linux-x64.tar.gz" -ErrorAction Stop
+        $ReleaseHash = (Get-FileHash -Algorithm SHA256 -Path "/tmp/powershell-$version-linux-x64.tar.gz" -ErrorAction Stop | Select-Object -ExpandProperty Hash).ToLower()
+    }
+    catch {
+        throw 
+    }
+    
+    $packageJson = Get-Content -Raw -Path "$PSScriptRoot/com.microsoft.powershell.json" | ConvertFrom-Json -Depth 100
+
+    #Update Package URL and Hash
+    $packageJson.modules.sources.url    = $packageUrl
+    $packageJson.modules.sources.sha256 = $ReleaseHash
+
+    #Update Flatpak JSON file 
+    $packageJson | ConvertTo-Json -Depth 100 | Set-Content -Path "$PSScriptRoot/com.microsoft.powershell.json" -Force
+
     try {
         sudo flatpak-builder --verbose ./build-dir com.microsoft.powershell.json --force-clean --repo=repo
         flatpak build-bundle -v ./repo powershell.flatpak com.microsoft.powershell
+    }
+    finally {
+        pop-location
+    }
+}
+
+If ($Install.IsPresent) {
+    Push-Location
+    Set-Location $PSScriptRoot
+
+    if(Test-Path -Path "$PSScriptRoot/powershell.flatpak"){
+        try {
+            sudo flatpak install -y powershell.flatpak
+        }
+        finally {
+            pop-location
+        }
+    }
+}
+
+If ($Uninstall.IsPresent) {
+    Push-Location
+    Set-Location $PSScriptRoot
+
+    try {
+        sudo flatpak uninstall -y powershell
     }
     finally {
         pop-location
